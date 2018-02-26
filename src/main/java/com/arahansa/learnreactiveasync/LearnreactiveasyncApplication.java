4ddb8b280a38bf5665467ec6c6ebdbc2b9c12b7f
arahansa <arahansa@naver.com>
2018-02-26 23:38:21 +0900

    first commit

---
+++ src/main/java/com/arahansa/learnreactiveasync/LearnreactiveasyncApplication.java
@@ -0,0 +1,12 @@
+package com.arahansa.learnreactiveasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnreactiveasyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnreactiveasyncApplication.class, args);
+	}
+}
