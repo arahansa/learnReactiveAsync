2820b96a11f58b54a41a95c99237d64e3a2e94fd
arahansa <arahansa@naver.com>
2018-02-27 00:08:15 +0900

    add async basic java

--- src/test/java/com/arahansa/learnreactiveasync/async/AsyncTestOne.java
+++ src/test/java/com/arahansa/learnreactiveasync/async/AsyncTestOne.java
@@ -1,4 +1,38 @@
 package com.arahansa.learnreactiveasync.async;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://www.youtube.com/watch?v=5KttCnoWLhs
+
 public class AsyncTestOne {
+
+    static class Result{
+        String s;
+        public Result(String s) {this.s = s;}
+        @Override
+        public String toString() {return "Result{ s='" + s + '\'' + '}';}
+    }
+
+    @Test
+    public void asyncTest() throws Exception{
+        Map map = new HashMap<>();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Result result = getApi();
+                map.put("API", result);
+            }
+        });
+
+        thread.start();
+        System.out.println(map.get("API")); // 여기서 NULL
+    }
+
+    private Result getApi() {
+        return new Result("s");
+    }
 }
--- src/test/java/com/arahansa/learnreactiveasync/async/AsyncTestTwo.java
+++ src/test/java/com/arahansa/learnreactiveasync/async/AsyncTestTwo.java
@@ -1,4 +1,43 @@
 package com.arahansa.learnreactiveasync.async;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://www.youtube.com/watch?v=5KttCnoWLhs
+
 public class AsyncTestTwo {
+
+    static class Result{
+        String s;
+        public Result(String s) {this.s = s;}
+        @Override
+        public String toString() {return "Result{ s='" + s + '\'' + '}';}
+    }
+
+    @Test
+    public void asyncTest() throws Exception{
+        Map map = new HashMap<>();
+
+        Thread thread = new Thread(() -> {
+            Result result = getApi();
+            map.put("API", result);
+            synchronized (map){
+                map.notify();
+            }
+        });
+
+        thread.start();
+        if(map.get("API")==null){
+            synchronized (map){
+                map.wait();
+            }
+        }
+        System.out.println(map.get("API"));
+    }
+
+    private Result getApi() {
+        return new Result("s");
+    }
 }
